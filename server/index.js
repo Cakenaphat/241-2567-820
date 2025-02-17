c3ad7b5c4f6395fa3a1d956f3947f28382cfad99
Cakenaphat <naphat.prap@ku.th>
2025-02-17 17:10:46 +0700

    update17/02/68

--- server/index.js
+++ server/index.js
@@ -1,66 +1,101 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
 const app = express();
 
-app.use(bodyParser.text());
+app.use(bodyParser.json());
 const port = 8000;
 
-// เก็บ user
 let users = []
-let counter = 1
+// let counter = 1;  [ -- NO LONGER USED -- ]
+let conn = null
 
+ const initMySQL = async () => {
+    conn = await mysql.createConnection({
+        host:'localhost',
+        user:     'root',  
+        password: 'root',
+        database: 'webdb',
+        port: 8830
+    })
+ }
 
-
-// path = Get /users
-app.get('/', (req,res) => {
-    res.json(users);
+ app.get('/testdb-new', async (req, res) => {
+    try{     
+        const results = await conn.query('SELECT * FROM users')
+         res.json(results[0])    
+    } catch(error){     
+        console.log('Error fetching users:', error.message)
+        res.status(500).json({error: 'Error fetching users'})
+    }
+ })
+ 
+// Path = / GET / Users
+app.get('/users', async (req,res) => {
+ // res.json(users); [ -- NO LONGER USED -- ]
+    const result = await conn.query('SELECT * FROM users')
+    res.json(result[0])
 })
 
-//path = POST /user
-app.post('/user', (req,res) => {
+// path = POST / User
+app.post('/user', async (req,res) => {
     let user = req.body;
-    user.id = counter
-    counter += 1
-    users.push(user);
+    const results = await conn.query('INSERT INTO users SET ?', user)
+    console.log('results', results)
     res.json({
-        message :'User created',
-        user : user
+        message: 'User Created',
+        data: results
     });
 })
-//path = PUT /user/:id
-app.put('user/:id', (req,res) => {
+
+// path = PUT / user/:id
+app.put('/user/:id', (req,res) => {
     let id = req.params.id;
-    //หา index ของ user ที่ต้องการ update
+    let updateUser = req.body;
+    // find users from id request
     let selectedIndex = users.findIndex(user => user.id == id)
-    // update ข้อมูล user 
-    if (updateUser.firstname) {
+    // update user 
+    if (updateUser.firstname){
         users[selectedIndex].firstname = updateUser.firstname
     }
+    if (updateUser.lastname){
+        users[selectedIndex].lastname = updateUser.lastname
+    }
+
     users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
-    users[selectedIndex].lastname = updateUser.lastname  || users[selectedIndex].lastname
+    users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
 
     res.json({
-        message : "User update",
-        data : {
-            user : updateUser,
-            indexUpdate : selectedIndex
+        message: 'User updated successfully',
+        data: {
+            user: updateUser,
+            indexUpdate: selectedIndex
         }
     });
-})
-//path = Delete /user/:id
+    // sent user info in update to where it belongs
+    /* 
+    GET / USERS = get all users
+    POST / USERS = create new user in data
+    GET /users/:id = get user by id
+    PUT /users/:id = get user by id
+    */
+ })
+
+// Path = DELETE / user/:id
 app.delete('/user/:id', (req,res) => {
     let id = req.params.id;
-    //หา index ของ user ที่ต้องการลบ
+    // find index of user
     let selectedIndex = users.findIndex(user => user.id == id)
-
-    users.splice(selectedIndex, 1)
+    
+    users.splice(selectedIndex,1)
     delete users[selectedIndex]
     res.json({
-        message : 'Delete Complete',
+        message: 'Delete Completed',
         indexDelete: selectedIndex
     });
 });
 
-app.listen(port,(req,res) =>{
-    console.log('Server is running on port' + port);
+app.listen(port, async () => {
+    await initMySQL();
+    console.log(`Server is running on port`+ port);
 });
